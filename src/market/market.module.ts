21acaee7528faa89d54ffd9f5b35002cee5458aa
TarasBrilian <cingihimutbanget@gmail.com>
2026-01-30 19:39:59 +0700

    chore: remove commented imports in market module

--- src/market/market.module.ts
+++ src/market/market.module.ts
@@ -5,14 +5,14 @@ import { MarketService } from './market.service';
 import { Market } from './entities/market.entity';
 import { TokensModule } from '../tokens/tokens.module';
 import { Order } from '../orders/entities/order.entity';
-import { Portfolio } from '../analytics/entities/portfolio.entity';
-import { BorrowPosition } from '../analytics/entities/borrow-position.entity';
+// import { Portfolio } from '../analytics/entities/portfolio.entity';
+// import { BorrowPosition } from '../analytics/entities/borrow-position.entity';
 import { CoreModule } from '../core/core.module';
 import { Token } from '../tokens/entities/token.entity';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Market, Order, Portfolio, BorrowPosition, Token]),
+        TypeOrmModule.forFeature([Market, Order, Token]),
         TokensModule,
         CoreModule,
     ],

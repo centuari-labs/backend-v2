fdbcdb4a595648d5d13d3e681905dac0ddc8f1c3
TarasBrilian <cingihimutbanget@gmail.com>
2026-01-30 19:42:39 +0700

    chore: remove commented imports in market module

--- src/market/market.module.ts
+++ src/market/market.module.ts
@@ -5,8 +5,6 @@ import { MarketService } from './market.service';
 import { Market } from './entities/market.entity';
 import { TokensModule } from '../tokens/tokens.module';
 import { Order } from '../orders/entities/order.entity';
-// import { Portfolio } from '../analytics/entities/portfolio.entity';
-// import { BorrowPosition } from '../analytics/entities/borrow-position.entity';
 import { CoreModule } from '../core/core.module';
 import { Token } from '../tokens/entities/token.entity';
 

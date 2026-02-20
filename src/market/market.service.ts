c1337cea33ad7adb10a3eaee6e313e5be1b05a9d
hwisesa23 <113232348+hwisesa23@users.noreply.github.com>
2026-02-20 22:10:26 +0700

    feat: Enhance market data structure with additional asset information
    - Updated the market data structure to include a placeholder for asset image URLs.
    - Added a TODO comment to implement functionality for returning the earliest market ID of maturity available for the assets.

--- src/market/market.service.ts
+++ src/market/market.service.ts
@@ -68,8 +68,9 @@ export class MarketService {
                     name: asset.name,
                     symbol: asset.symbol,
                     decimals: asset.decimals ?? null,
+                    //@todo : should return image url of the asset
                 },
-                markets: {
+                markets: { //@todo : should return the earliest market id of maturity available of the assets
                     market_id: this.marketRepository.getMarketId(asset.id),
                     maturity: new Date().toISOString(),
                 },

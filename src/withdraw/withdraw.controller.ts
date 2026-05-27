fd9013f1d961f1e8ef6b7704ef2c4e03418f111f
hwisesa23 <113232348+hwisesa23@users.noreply.github.com>
2026-05-27 23:24:09 +0700

    refactor: streamline import statements in WithdrawController
    - Consolidated import statements for WithdrawRequestDto and WithdrawResponseDto in withdraw.controller.ts for improved readability and maintainability.

--- src/withdraw/withdraw.controller.ts
+++ src/withdraw/withdraw.controller.ts
@@ -2,10 +2,7 @@ import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { Wallet } from "../common/decorators/wallet.decorator";
 import { AuthGuard } from "../common/guards/auth.guard";
 import { WithdrawService } from "./withdraw.service";
-import type {
-    WithdrawRequestDto,
-    WithdrawResponseDto,
-} from "./dto/withdraw.dto";
+import { WithdrawRequestDto, type WithdrawResponseDto } from "./dto/withdraw.dto";
 
 @Controller("withdraw")
 export class WithdrawController {
